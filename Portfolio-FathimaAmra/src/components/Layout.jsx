6de0e36db8476e6901c4871c78d69a0d7bfec468
Amra Badurdeen <fmoha239@my.centennialcollege.ca>
2025-09-24 02:41:22 -0400

    New updates
    Nav bar style added
    logo added
    img folder created

--- Portfolio-FathimaAmra/src/components/Layout.jsx
+++ Portfolio-FathimaAmra/src/components/Layout.jsx
@@ -1,17 +1,26 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/img/logo.png";
 
 export default function Layout() {
     return (
         <>
-        <h1>My Portfolio</h1>
-            <nav>
-                <Link to="/">Home</Link>
-                <Link to="/about">About</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/education">Education</Link>
-                <Link to="/services">Services</Link>
-                <Link to="/contact">Contact</Link>
-            </nav>
+            <div className="layout">
+                <nav className="navbar">
+                    <div className="logo">
+                        <Link to="/">
+                            <img src={logo} alt="Logo" />
+                        </Link>
+                    </div>
+                    <ul className="nav-links">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/about">About</Link></li>
+                        <li><Link to="/projects">Projects</Link></li>
+                        <li><Link to="/education">Education</Link></li>
+                        <li><Link to="/services">Services</Link></li>
+                        <li><Link to="/contact">Contact</Link></li>
+                    </ul>
+                </nav>
+            </div>
         </>
     )
 }
