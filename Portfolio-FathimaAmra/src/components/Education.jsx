c96f0be1c2b7b8f633584fdca8cacbd5b2846b0e
Amra Badurdeen <fmoha239@my.centennialcollege.ca>
2025-09-24 14:15:34 -0400

    Education content and css update
    Education content added and Index.css updated with the css styles

--- Portfolio-FathimaAmra/src/components/Education.jsx
+++ Portfolio-FathimaAmra/src/components/Education.jsx
@@ -1,7 +1,31 @@
 export default function Education() {
     return (
         <>
-            <h1>Hello education</h1>
+            <section class="education">
+                <h2>Education & Certifications</h2>
+
+                <div class="education-section">
+                    <h3>Advanced Diploma – Software Engineering Technology</h3>
+                    <p><strong>Institution:</strong> Centennial College, Toronto</p>
+                    <p><strong>Year: 2024</strong> Ongoing (Co-op Program)</p>
+                </div>
+                <div class="line"></div>
+
+                <div class="education-section">
+                    <h3>Bachelor’s in Software Engineering</h3>
+                    <p><strong>Institution:</strong> Cardiff Metropolitan University, United Kingdom</p>
+                    <p><strong>Year: 2016</strong> Completed</p>
+                </div>
+                <div class="line"></div>
+
+                <div class="education-section">
+                    <h3>Current Certification – Data Analysis & Visualization</h3>
+                    <p><strong>Platform:</strong> Coursera</p>
+                    <p><strong>Year: 2025</strong> Ongoing</p>
+                </div>
+
+            </section>
         </>
-    );
+    )
 }
+
