3de2e2a81fc35d6561896ce2e00c26bedc8c5b6f
Amra Badurdeen <fmoha239@my.centennialcollege.ca>
2025-09-24 18:16:14 -0400

    Deployment configuration implemented
    changes done to package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: { 
+    allowedHosts: ['web-comp229-amra-fall2025.onrender.com'] 
+  }
 })
